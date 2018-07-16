36078f84b085db67e878175952610a4df1ff8029
Alexz <rumblesoso7@gmail.com>
2018-07-16 16:07:46 +0800

    learn

--- citysearch/src/app/highlight.pipe.ts
+++ citysearch/src/app/highlight.pipe.ts
@@ -6,6 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class StrongPipe implements PipeTransform {
     transform(text: string, searchStr): string {
-        return text.replace(new RegExp(searchStr, 'gi'), `<strong>${searchStr}</strong>`)
+        return text.replace(new RegExp(searchStr, 'gi'), `<strong>${searchStr}</strong>`);
     }
 }
